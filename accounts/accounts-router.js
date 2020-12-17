696b605bc83d3febdcfa1ab8e9f4d095e38873a3
Mychael M <mychaelbmenges@gmail.com>
2020-12-16 17:09:12 -0700

    added endpoint to get account by id

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -4,11 +4,21 @@ const db = require("../data/dbConfig.js");
 
 const router = express.Router();
 
-router.use("/", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     // SELECT * FROM accounts
     const accounts = await db.select('*').from('accounts');
     res.json(accounts);
+  } catch(err) {
+    next(err);
+  }
+})
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    // SELECT * FROM accounts WHERE id = ?
+    const account = await db("accounts").first().where("id", req.params.id);
+    res.json(account);
   } catch(err) {
     next(err);
   }
