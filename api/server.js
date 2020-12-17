d086d89aee48296ed9ac81df1e95fb707a00e2c8
Mychael M <mychaelbmenges@gmail.com>
2020-12-16 17:00:18 -0700

    set up welcome route and error routes in server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const accountsRouter = require('../accounts/accounts-router');
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/accounts", accountsRouter);
+
+server.get("/", async (req, res, next) => {
+  try {
+    res.json({
+      message: "Welcome to the server",
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
 
 module.exports = server;
