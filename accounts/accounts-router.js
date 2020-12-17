098e36c027679cba26d37a46554d4b2e66535915
Mychael M <mychaelbmenges@gmail.com>
2020-12-16 17:17:25 -0700

    added endpoint to create new account and return new account

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -24,4 +24,19 @@ router.get("/:id", async (req, res, next) => {
   }
 })
 
+router.post("/", async (req, res, next) => {
+  try {
+    // INSERT INTO accounts (name, budget) VALUES (?, ?)
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    }
+    const [id] = await db.insert(payload).into("accounts");
+    const account = await db("accounts").first().where("id", id)
+    res.status(201).json(account);
+  } catch(err) {
+    next(err);
+  }
+})
+
 module.exports = router;
